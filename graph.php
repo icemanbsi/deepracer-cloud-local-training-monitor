45686614ae8f11fe1aa5fbe6536ec2cf060b01de
Bobby Stenly <iceman.bsi@gmail.com>
2019-08-20 15:41:34 +0700

    first init

--- graph.php
+++ graph.php
@@ -9,11 +9,12 @@ $type = $_GET["type"];
 // }
 
 if($output){
-	foreach($output as $key=>$value){
-	    if($key==1)
-	        print chr(0x0D); //Newline feed after PNG declaration
-	    if($key>0)
-	        print "\n";
-	    print $value;
-	}
+	print $output;
+	// foreach($output as $key=>$value){
+	//     if($key==1)
+	//         print chr(0x0D); //Newline feed after PNG declaration
+	//     if($key>0)
+	//         print "\n";
+	//     print $value;
+	// }
 }
