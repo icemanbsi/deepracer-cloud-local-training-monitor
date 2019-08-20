d8f4ff4f356b20393b5633222d3d02369488bd73
Bobby Stenly <iceman.bsi@gmail.com>
2019-08-20 14:19:05 +0700

    first init

---
+++ dashboard.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		
+	</head>
+	<body>
+		<h3>Reward Distribution</h3>
+		<img src="graph.php?type=reward"/>
+	</body>
+</html>
---
+++ graph.php
@@ -0,0 +1,19 @@
+<?php 
+header("Content-type: image/png");
+$type = $_GET["type"];
+if($type == "reward"){
+	$graph = exec('python py/reward.py', $output);
+}
+else{
+
+}
+
+if($output){
+	foreach($output as $key=>$value){
+	    if($key==1)
+	        print chr(0x0D); //Newline feed after PNG declaration
+	    if($key>0)
+	        print "\n";
+	    print $value;
+	}
+}
