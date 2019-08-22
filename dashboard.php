f684a3229a3aba095dc9bb9b3287d7c04cf5ec47
Bobby Stenly <iceman.bsi@gmail.com>
2019-08-22 22:22:41 +0700

    entropy

--- dashboard.php
+++ dashboard.php
@@ -3,17 +3,9 @@
 		
 	</head>
 	<body>
-		<table width="100%">
-		<tr>
-			<td width="50%">
-				<h2>Reward Distribution</h2>
-				<img src="graph.php?type=reward"/>
-			</td>
-			<td width="50%">
-				<h2>Entropy Distribution</h2>
-				<img src="graph.php?type=entropy"/>
-			</td>
-		</tr>
-		</table>
+		<h2>Reward Distribution</h2>
+		<img src="graph.php?type=reward"/>
+		<h2>Entropy Distribution</h2>
+		<img src="graph.php?type=entropy"/>
 	</body>
 </html>
