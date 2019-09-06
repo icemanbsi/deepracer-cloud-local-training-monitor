e80c7163b65f7401a80c1159f0e4fb956c824ccd
Bobby Stenly <iceman.bsi@gmail.com>
2019-09-06 22:56:18 +0700

    update write settings

--- write-settings.php
+++ write-settings.php
@@ -4,7 +4,7 @@ echo "opening file <br/>";
 $handle = fopen($configFile, 'w') or die('Cannot open file:  '.$configFile);
 echo "file opened <br/>";
 $data_json["num_episodes_between_training"] = intval($_POST["num_episodes_between_training"]);
-$data_json["robomaker_log_path"] = $_POST["robomaker_log_path"]);
+$data_json["robomaker_log_path"] = $_POST["robomaker_log_path"];
 $data_json["sagemaker_log_path"] = $_POST["sagemaker_log_path"];
 $data_string = json_encode($data_json);
 echo "writing file <br/>";
